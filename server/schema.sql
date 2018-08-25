6ea162ffd49c61fa66576e270defea8ef97a46fa
cstung21 <cstung21@gmail.com>
2018-08-24 19:59:08 -0700

    completed schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+/* Create other tables and define schemas for them here! */
+
+CREATE TABLE users (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(25),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+
+CREATE TABLE chatrooms (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(25),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT,
+  text VARCHAR(255),
+  chatroom_id INT,
+  created_at DATETIME,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (chatroom_id) REFERENCES chatrooms(id)
+);
 
 
 
